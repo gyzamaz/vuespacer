5588d1c5ed1bf9b86808b6948168b5452a337519
gyzamaz <konradkadzielawa@gmail.com>
2019-03-17 00:30:31 +0100

    search with api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome"/>
+    <Search />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Search from "../components/Search";
 
 export default {
   name: 'home',
   components: {
+    Search,
     HelloWorld,
   },
 };
